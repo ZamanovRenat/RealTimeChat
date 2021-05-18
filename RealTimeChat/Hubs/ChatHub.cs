043d7a9d88c11492a3e2c9b6f19c2aa4a493a8d0
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-18 18:30:48 +0300

    Доработан метод подключения пользователей в чат, подключен SignalR в классе startup

--- RealTimeChat/Hubs/ChatHub.cs
+++ RealTimeChat/Hubs/ChatHub.cs
@@ -14,9 +14,15 @@ namespace RealTimeChat.Hubs
         {
             _botUser = "MyChat Bot";
         }
+        /// <summary>
+        /// Подключение пользователей к чату
+        /// </summary>
+        /// <param name="userConnection"></param>
+        /// <returns></returns>
         public async Task JoinRoom(UserConnection userConnection)
         {
-            await Clients.All.SendAsync("ReceiveMessage", _botUser, 
+            await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, 
                 $"{userConnection.User} has joined {userConnection.Room}");
         }
     }
