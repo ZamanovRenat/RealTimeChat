683b10575e3334501b4979661c6e77fc066bd091
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-18 18:23:02 +0300

    Добавлены класс ChatHub и свойства Room и User

---
+++ RealTimeChat/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly string _botUser;
+        public ChatHub()
+        {
+            _botUser = "MyChat Bot";
+        }
+        public async Task JoinRoom(UserConnection userConnection)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", _botUser, 
+                $"{userConnection.User} has joined {userConnection.Room}");
+        }
+    }
+}
---
+++ RealTimeChat/UserConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealTimeChat
+{
+    public class UserConnection
+    {
+        public string User { get; set; }
+        public string Room { get; set; }
+    }
+}
