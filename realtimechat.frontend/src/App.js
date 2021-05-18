6dafec808c16408edfc6c77cafe7842ac93d7108
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-18 21:00:23 +0300

    Создал класс подключения к чату

--- realtimechat.frontend/src/App.js
+++ realtimechat.frontend/src/App.js
@@ -1,26 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Lobby from './components/Lobby';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const joinRoom = async (user, room) => {
+    try {
+
+    } catch (e) 
+    {
+      console.log(e);
+    }
+  }
+  return <div className='app'>
+    <h2>Real Time Chat</h2>
+    <hr className='line' />
+    <Lobby joinRoom={joinRoom} />
+  </div>
 }
 
 export default App;
