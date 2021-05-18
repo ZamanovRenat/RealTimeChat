1d7e384ce7ff46f43a848ec2ccecff0b73c0427f
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-18 22:00:50 +0300

    Добавлен класс Chat

--- realtimechat.frontend/src/App.js
+++ realtimechat.frontend/src/App.js
@@ -1,36 +1,66 @@
+import { useState } from 'react';
+import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
+import Lobby from './components/Lobby';
+import Chat from './components/Chat';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Lobby from './components/Lobby';
-import { HubConnectionBuilder, LogLevel } from '@microsoft/signalr';
-import { configure } from '@testing-library/dom';
-import { useState } from 'react';
 
-//подключение к чату
 const App = () => {
   const [connection, setConnection] = useState();
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
   const joinRoom = async (user, room) => {
     try {
       const connection = new HubConnectionBuilder()
-      .withUrl("https://localhost:44300/chat")
-      .configureLogging(LogLevel.Information)
-      .build();
+        .withUrl("https://localhost:44300/chat")
+        .configureLogging(LogLevel.Information)
+        .build();
 
       connection.on("ReceiveMessage", (user, message) => {
-        console.log('message received: ', message);
-      })
+        setMessages(messages => [...messages, { user, message }]);
+      });
+
+      connection.on("UsersInRoom", (users) => {
+        setUsers(users);
+      });
+
+      connection.onclose(e => {
+        setConnection();
+        setMessages([]);
+        setUsers([]);
+      });
 
       await connection.start();
-      await connection.invoke("JoinRoom", {user, room});
+      await connection.invoke("JoinRoom", { user, room });
       setConnection(connection);
-    } catch (e) 
-    {
+    } catch (e) {
       console.log(e);
     }
   }
+
+  const sendMessage = async (message) => {
+    try {
+      await connection.invoke("SendMessage", message);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const closeConnection = async () => {
+    try {
+      await connection.stop();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   return <div className='app'>
-    <h2>Real Time Chat</h2>
+    <h2>MyChat</h2>
     <hr className='line' />
-    <Lobby joinRoom={joinRoom} />
+    {!connection
+      ? <Lobby joinRoom={joinRoom} />
+      : <Chat sendMessage={sendMessage} messages={messages} users={users} closeConnection={closeConnection} />}
   </div>
 }
 
--- realtimechat.frontend/src/components/Lobby.js
+++ realtimechat.frontend/src/components/Lobby.js
@@ -1,23 +1,22 @@
-import { Button } from "bootstrap"
-import { useState } from "react"
-import { Form } from "react-bootstrap"
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
 
-//вход в чат
+//Вход в чат
 const Lobby = ({ joinRoom }) => {
-     const [user, setUser] = useState();
-     const [room, setRoom] = useState();
-     
-     return <Form className='lobby'
+    const [user, setUser] = useState();
+    const [room, setRoom] = useState();
+
+    return <Form className='lobby'
         onSubmit={e => {
             e.preventDefault();
             joinRoom(user, room);
-        }}>
-         <Form.Group>
-             <Form.Control placeholder='name' onChange={e => setUser(e.target.value)} />
-             <Form.Control placeholder='room' onChange={e => setRoom(e.target.value)} />
-         </Form.Group>
-         <Button variant='success' type='submit' disabled={!user || !room}>Join</Button>
-     </Form>
+        }} >
+        <Form.Group>
+            <Form.Control placeholder="name" onChange={e => setUser(e.target.value)} />
+            <Form.Control placeholder="room" onChange={e => setRoom(e.target.value)} />
+        </Form.Group>
+        <Button variant="success" type="submit" disabled={!user || !room}>Join</Button>
+    </Form>
 }
 
 export default Lobby;
