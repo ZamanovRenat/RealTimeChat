a61e0aa093abe276031da5f0eeb94da794b431cd
ZamanovRenat <zamanov.renat@mail.ru>
2021-05-18 21:59:02 +0300

    добавление CORS  в back

--- RealTimeChat/Startup.cs
+++ RealTimeChat/Startup.cs
@@ -13,11 +13,25 @@ namespace RealTimeChat
 {
     public class Startup
     {
+        //private Action<CorsOptions> options;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
+
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:3000/")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod()
+                    .AllowCredentials();
+
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,6 +43,8 @@ namespace RealTimeChat
             }
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseEndpoints(endpoints =>
             {
